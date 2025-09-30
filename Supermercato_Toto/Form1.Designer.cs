55e0f029b57dba2f6ecf25c64f464a80c842e2b6
12999441 <12999441@itisrossi.vi.it>
2025-09-30 11:00:56 +0200

    file json

--- Supermercato_Toto/Form1.Designer.cs
+++ Supermercato_Toto/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.dtg_catalogo = new System.Windows.Forms.DataGridView();
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dtg_catalogo)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,7 +41,7 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(776, 55);
+            this.label1.Size = new System.Drawing.Size(447, 55);
             this.label1.TabIndex = 0;
             this.label1.Text = "Supermercato Totò";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -62,11 +64,33 @@
             this.label2.Text = "Catalogo";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button1.Location = new System.Drawing.Point(378, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 54);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Rifornisci";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button2.Location = new System.Drawing.Point(378, 152);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 54);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Aggiungi al carrello";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.dtg_catalogo);
             this.Controls.Add(this.label1);
@@ -83,6 +107,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dtg_catalogo;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
